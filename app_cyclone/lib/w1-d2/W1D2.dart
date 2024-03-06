34ede6798ecb1fc76530112c5666ff0b46c77ab2
vhyenn <84668078+VienHaiYen@users.noreply.github.com>
2024-03-06 15:35:24 +0700

    [W1-D2] Stateless Widget, Stateful Widget, Lifecycle

--- app_cyclone/lib/w1-d2/W1D2.dart
+++ app_cyclone/lib/w1-d2/W1D2.dart
@@ -1,5 +1,4 @@
 import "package:flutter/material.dart";
-import "package:flutter/widgets.dart";
 import "package:cached_network_image/cached_network_image.dart";
 import "package:flutter_svg/flutter_svg.dart";
 
@@ -8,6 +7,22 @@ class W1D2 extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView(
       children: [
+        // ImageDisplayExample(),
+        StateExample()
+      ],
+    );
+  }
+}
+
+class ImageDisplayExample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          "Assets & Image",
+          style: TextStyle(fontSize: 25),
+        ),
         Text("Image from Network by CachedNetworkImage:"),
         CachedNetworkImage(
           imageUrl:
@@ -22,6 +37,106 @@ class W1D2 extends StatelessWidget {
         Text("SVG Flutter:"),
         SvgPicture.asset('assets/svg/w1-d2.svg', height: 300),
       ],
+    );
+  }
+}
+
+class StateExample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Text(
+          "Stateless Widget, Stateful Widget, Lifecycle",
+          style: TextStyle(fontSize: 25),
+        ),
+        StatelessExamples(),
+        StatefulExample(
+          initCounter: 1,
+        )
+      ],
+    );
+  }
+}
+
+class StatelessExamples extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      color: Colors.blue[100],
+      margin: EdgeInsets.all(20),
+      padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+      child: const Column(
+        children: [
+          Text(
+            "StatelessWidget",
+            style: TextStyle(fontSize: 20),
+          ),
+          Text(
+              "Đây là một Widget tĩnh, không thay đổi những gì nó hiển thị sau khi render"),
+        ],
+      ),
+    );
+  }
+}
+
+class StatefulExample extends StatefulWidget {
+  final initCounter;
+  const StatefulExample({Key? key, required this.initCounter});
+
+  @override
+  _StatefulExampleState createState() => _StatefulExampleState();
+}
+
+class _StatefulExampleState extends State<StatefulExample> {
+  late int _count = widget.initCounter;
+
+  void increment() {
+    setState(() {
+      _count++;
+    });
+  }
+
+  void decrement() {
+    setState(() {
+      _count--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      color: Color.fromARGB(255, 225, 166, 236),
+      margin: EdgeInsets.all(20),
+      padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+      child: Column(
+        children: [
+          const Text(
+            "StatefulWidget",
+            style: TextStyle(fontSize: 20),
+          ),
+          const Text(
+              "Đây là một Widget có thể thay đổi những gì nó hiển thị sau khi render bằng cách thay đổi state của chính nó"),
+          Text("Count: $_count"),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                  onPressed: increment, child: const Text("Increment")),
+              const SizedBox(
+                width: 10,
+              ),
+              ElevatedButton(
+                  onPressed: decrement, child: const Text("Decrement")),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
